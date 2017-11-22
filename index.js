5cbd2d962fdf15d51ffc81b073a59cbb6d2f175b
Paul Winkler <paul.winkler@homeoffice.gov.uk>
2017-11-22 16:09:38 +0000

    Send more data before wait

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ function sendData(res, meg) {
     for (k = 0; k < meg * 1024; k++) {
         res.write(Buffer.from(new Array(1024)));
     }
-    res.end();
 }
 
 app.get('/PHOTOBIG', (req, res, next) => {
     sendHeader(res);
     sendMime(res);
     sendData(res, 11);
+    res.end();
 });
 
 app.get('/PHOTOSLW', (req, res, next) => {
@@ -33,15 +33,18 @@ app.get('/PHOTOSLW', (req, res, next) => {
         sendHeader(res);
         sendMime(res);
         sendData(res, 1);
+        res.end();
     }, 40000);
 });
 
 app.get('/PHOTOBDY', (req, res, next) => {
     sendHeader(res);
     sendMime(res);
+    sendData(res, 2);
     res.flush();
     setTimeout(() => {
         sendData(res, 1);
+        res.end();
     }, 40000);
 });
 
